1154d6d975b567d427c767888c8865ea9017f246
Maayan Brodsky <maayanbrodsky@gmail.com>
2021-01-09 16:49:19 -0500

    Second commit

--- upload_284.py
+++ upload_284.py
@@ -1,22 +1,28 @@
-from typing import Dict, List
+from typing import List
 
 PATH = r'C:\Users\maaya\Dropbox\Python\upload_284 - Donald\accounts.txt'
 
-def parse_file(path: str) -> Dict[int, int]:
+def parse_file(path: str) -> List[int]:
+    """Takes a file path, reads the txt file and returns a list
+    of integers in order and according to positive or negative state."""
     with open(path, 'r') as file:
         accounts: List[str] = file.readlines()
     cleaned_accounts = [account.rstrip('\r\n') for account in accounts]
-    formatted_accounts = {}
+    formatted_accounts = []
     for i, account in enumerate(cleaned_accounts):
         if account[0] == '+':
-           formatted_accounts[i] = int(account[1:])
+           formatted_accounts.append(int(account[1:]))
         if account[0] == '-':
-           formatted_accounts[i] = int(account[1:]) * - 1
+           formatted_accounts.append(int(account[1:]) * -1)
     return formatted_accounts
 
 
-def very_rich(accounts: Dict[int, int]):
+def very_rich(accounts: List[int], first_day: int, last_day: int) -> int:
+    """Takes parsed data and first and last day slices the list and returns the sum."""
+    return sum(accounts[first_day - 1:last_day])
 
 
 
 accounts = parse_file(PATH)
+print(accounts)
+print(very_rich(accounts, 3, 6))
